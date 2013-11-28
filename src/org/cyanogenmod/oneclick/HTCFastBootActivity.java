dcf70ff8fabca72a1624ad99dd8c4ed412e97776
Frieder Bluemle <info@bluemle.net>
2013-11-27 21:35:38 -0800

    Remove unused import

--- src/org/cyanogenmod/oneclick/HTCFastBootActivity.java
+++ src/org/cyanogenmod/oneclick/HTCFastBootActivity.java
@@ -4,7 +4,6 @@ import android.app.Activity;
 import android.content.ActivityNotFoundException;
 import android.content.ComponentName;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
