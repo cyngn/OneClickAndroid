b03ccc60a1bf7ab9bb1bc90a059ed40b5b822247
Frieder Bluemle <frieder.bluemle@gmail.com>
2014-01-04 02:47:26 -0800

    Add Apache license file headers

--- src/org/cyanogenmod/oneclick/HTCFastBootMonitorService.java
+++ src/org/cyanogenmod/oneclick/HTCFastBootMonitorService.java
@@ -1,12 +1,25 @@
+/*
+ * Copyright 2014 Cyanogen, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.cyanogenmod.oneclick;
 
 import android.app.Activity;
 import android.content.ComponentName;
 import android.provider.Settings;
 
-/**
- * Created by koush on 10/3/13.
- */
 public class HTCFastBootMonitorService extends MonitorService {
     ComponentName htcPowerManager = new ComponentName("com.htc.htcpowermanager", "com.htc.htcpowermanager.PowerManagerActivity");
 
